53d6a4ecfd07c669ef7eb1c06afc8884b3d0180c
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2026-03-29 19:04:08 -0300

    refactor: Configure Prisma client with PostgreSQL adapter and connection string

--- server/config/database/prisma/index.ts
+++ server/config/database/prisma/index.ts
@@ -1,3 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { env } from "../../env";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: env.DATABASE_URL }),
+});
