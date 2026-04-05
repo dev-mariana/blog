b1c8893cebcb63684c64444563628a4ba83367cb
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2026-04-05 15:17:44 -0300

    feat: add index page with router redirection to blogs

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-page />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+if (process.client) {
+  router.replace("/blogs");
+}
+</script>
+
+<style scoped></style>
