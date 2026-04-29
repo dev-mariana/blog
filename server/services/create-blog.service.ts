dc31beafb33036110cb55edab36dfde92927d07f
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2026-04-28 22:21:25 -0300

    feat: implement slug uniqueness validation and auto-incrementing for new posts

--- server/services/create-blog.service.ts
+++ server/services/create-blog.service.ts
@@ -5,7 +5,16 @@ export class CreateBlogService {
   constructor(private readonly blogsRepository: IBlogsRepository) {}
 
   async execute(data: Partial<Post>): Promise<Post> {
-    const post = await this.blogsRepository.create(data);
+    const baseSlug = data.slug ?? "";
+    let slug = baseSlug;
+    let counter = 2;
+
+    while (await this.blogsRepository.findBySlug(slug)) {
+      slug = `${baseSlug}-${counter}`;
+      counter++;
+    }
+
+    const post = await this.blogsRepository.create({ ...data, slug });
     return post;
   }
 }
